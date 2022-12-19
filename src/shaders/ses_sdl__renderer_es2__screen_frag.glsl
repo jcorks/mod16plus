426a306d5bc68eabde1d89392e6162b055dd81aa
jcorks <jcorkery@umich.edu>
2022-12-19 11:19:43 -0500

    Added 16bit color output with dithering (needs proper "pixel" size). Added radio widget for further development views

--- src/shaders/ses_sdl__renderer_es2__screen_frag.glsl
+++ src/shaders/ses_sdl__renderer_es2__screen_frag.glsl
@@ -5,6 +5,43 @@ varying mediump vec2 uv_frag;
 uniform sampler2D sampler;
 
 
-void main() {   
-    gl_FragColor = texture2D(sampler, uv_frag);
+
+// 16-bit color with half-shade dithering
+
+
+
+bool is32shadel(mediump float a, mediump float b) {
+    return int(a * 64.0) < int(b * 64.0);
+}
+
+bool is64shadel(mediump float a, mediump float b) {
+    return int(a * 128.0) < int(b * 128.0);
+}
+
+
+void main() {  
+
+    mediump vec4 shadeReal = texture2D(sampler, uv_frag);
+
+    /// R5 G6 R5
+    mediump vec4 shade16 = vec4(
+        floor(shadeReal.r * 32.0) / 32.0,
+        floor(shadeReal.g * 64.0) / 64.0,
+        floor(shadeReal.b * 32.0) / 32.0,
+        1
+    );
+
+    int x = int(gl_FragCoord.x);
+    int y = int(gl_FragCoord.y);
+
+    // dither by ever other
+    mediump float offset = mod(float(x/4), 2.0) * mod(float(y/4), 2.0) * 1.0;
+
+    shade16.r += (is32shadel(shade16.r, shadeReal.r)) ? offset / 32.0 : 0.0;
+    shade16.g += (is64shadel(shade16.g, shadeReal.g)) ? offset / 64.0 : 0.0;
+    shade16.b += (is32shadel(shade16.b, shadeReal.b)) ? offset / 32.0 : 0.0;
+        //shade16.b = 1.0;
+
+    gl_FragColor = shade16;
+
 }
